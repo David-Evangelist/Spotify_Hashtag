34438e0012e099c126a3507b3e83ad797dd24577
David-Evangelist <dev.davidwilliam@gmail.com>
2025-02-16 18:21:28 -0300

    Ajuste no process env

--- front_end/api/api.js
+++ front_end/api/api.js
@@ -3,9 +3,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
+// const { NODE_ENV } = process.env;
 
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://spotify-hashtag-o3zj.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
